1ac9ca700478126184105383e8c0306cce92ac61
Yash <yashkapadiya2012@gmail.com>
2026-02-20 18:26:31 +0530

    first commit

---
+++ background.js
@@ -0,0 +1,31 @@
+
+
+const defaultSettings = {
+
+};
+
+
+chrome.runtime.onInstalled.addListener(details => {
+    if (details.reason === chrome.runtime.OnInstalledReason.INSTALL) {
+        chrome.storage.local.set(defaultSettings);
+    }
+    else if (details.reason === chrome.runtime.OnInstalledReason.UPDATE) {
+        chrome.storage.local.get(null, (existingSettings) => {
+            const newSettings = {
+                ...defaultSettings, ...existingSettings
+            }
+            chrome.storage.local.set(newSettings);
+        });
+    }
+});
+
+
+
+chrome.commands.onCommand.addListener((command, tab) => {
+    if (command === "search-current-page") {
+        chrome.tabs.sendMessage(tab.id, {
+            for: "search-current-page"
+        });
+    }
+});
+
